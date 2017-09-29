1b614e79e02835e994a8952a100af4e6bcb94f86
szy0syz <szy0syz@gmail.com>
2017-09-29 16:07:28 +0800

    fixed CH07

--- router/zf_CH07/src/components/UserAdd.js
+++ router/zf_CH07/src/components/UserAdd.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Prompt } from 'react-router-dom' // Prompt弹出框
 export default class UserAdd extends Component {
 
   constructor(props) {
@@ -16,36 +15,20 @@ export default class UserAdd extends Component {
     let users = usersStr ? JSON.parse(usersStr) : [];
     users.push({ id: Date.now(), name });
     localStorage.setItem('users', JSON.stringify(users));
-    // 这也玩回调，我也是醉了。
-    this.setState({blocking: false}, () => {
-      this.props.history.push('/user/list'); //跳转路由咯
-    });
-  };
-
-  handleChange = (e) => {
-    let inpValue = e.target.value;
-    this.setState({
-      blocking: Boolean(inpValue && inpValue.length > 0)
-    });
+    this.props.history.push('/user/list'); //跳转路由咯
   };
 
   render() {
     return ( // when属性无论如何都必须确保是Boolean类型的
-      <div>
-        <Prompt when={this.state.blocking}
-          message={(location) => `确定跳转到${location.pathname}吗？`}
-        />
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">用户名</label>
-            <input onClick={this.handleChange} type="text" ref={ref => this.name = ref} className="form-control" />
-          </div>
-          <div className="form-group">
-            <input className="btn btn-primary" type="submit" />
-          </div>
-        </form>
-      </div>
-
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">用户名</label>
+          <input type="text" ref={ref => this.name = ref} className="form-control" />
+        </div>
+        <div className="form-group">
+          <input className="btn btn-primary" type="submit" />
+        </div>
+      </form>
     );
   };
 }
---
+++ router/zf_CH08/src/components/MenuLink.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+// 这个组件，我们不管它是否匹配，都显示一些东西
+// 新的渲染方法：children
+// component 对应一个组件 当URL路径跟当前Route path匹配时渲染
+// render 对应一个匿名的组件函数 当URL路径跟当前Route path匹配时渲染
+// children 
+
+// export default function (props) {
+// li的className就可以放判断的表达式
+export default function ({ to, label, activeOnlyWhenExact }) {
+  // <Route path={to} children={(props) => {
+  // 这里结构props里的match了，匹配到路由match有值，没有匹配就没值 
+  return (
+    <Route path={to} exact={activeOnlyWhenExact} children={({ match }) => {
+      return <li className={match ? 'active' : ''}><Link to={to}>{label}</Link></li>
+    }} />
+  );
+}
