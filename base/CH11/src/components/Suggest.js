1253b281ba172d8fbec371dbb4e9359417289967
szy0syz <szy0syz@gmail.com>
2017-10-03 14:18:18 +0800

    finished CH11 demo 百度搜索框 React版

--- base/CH11/src/components/Suggest.js
+++ base/CH11/src/components/Suggest.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
-
+import jsonp from 'jsonp';
 export default class Suggest extends Component {
 
   constructor(){
     super();
     this.state = {
-      words: ['a', 'b', 'c']
+      words: [],
+      index: -1 //words数组的索引
     }
   }
 
   handleChange = (e) => {
     // 要不要考虑下IE
     let wd = e.target.value;
-    console.log(wd);
+    jsonp(`https://www.baidu.com/su?wd=${wd}`, {
+      param: 'cb'
+    }, (err, data) => {
+      this.setState({words: data.s});
+    });
   }
 
+  handleKeyDown = (e) => {
+    let code = e.keyCode;
+    if (code === 13) {
+      console.log('13');
+      window.location.href = `https://www.baidu.com/s?wd=${this.state.words[this.state.index]}`;
+      return;
+    }
+    // 当按下的是向上或向下键时
+    if (code === 38 || code === 40) {
+      let boundary = this.state.words.length -1;
+      let index = this.state.index;
+      if(code === 38) {
+        index--;
+        index = index < 0 ? boundary : index;
+      }
+      if(code === 40) {
+        index++;
+        index = index > boundary ? 0 : index;
+      }
+      this.setState({index});
+      this.refs.input.value = this.state.words[index];
+      
+    }
+  }
+  // value={this.state.words[this.state.index]}
   render() {
     return (
       <div className="container">
@@ -22,13 +52,13 @@ export default class Suggest extends Component {
           <div className="col-sm-8 col-sm-offset-2">
             <div className="panel panel-default">
               <div className="panel-heading">
-                <input type="text" onChange={this.handleChange} className="form-control"/>
+                <input ref="input" onKeyDown={this.handleKeyDown} type="text" onChange={this.handleChange} className="form-control"/>
               </div>
               <div className="panel-body">
                 <ul className="list-group">
                   {
                     this.state.words.map((word, index) => {
-                      return <li key={index} className="list-group-item">{word}</li>
+                      return <li key={index} className={"list-group-item " + (index === this.state.index ? 'active' : '')}>{word}</li>
                     })
                   }
                 </ul>
