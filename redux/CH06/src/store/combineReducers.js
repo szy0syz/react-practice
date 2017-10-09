4ae5d75fe299e1ce8bb1acee1a9889d07bba21fe
szy0syz <szy0syz@gmail.com>
2017-10-09 15:44:33 +0800

    updated 1.分离combineReducers模块和Clean code

---
+++ redux/CH06/src/store/combineReducers.js
@@ -0,0 +1,17 @@
+// 此时虽然我们有两个state的管理员reducer，但是createStore没法同时传入两个reducer，所以我们得给他们俩个找个“爹”。
+// 高阶函数
+let combineReducers = (reducers) =>
+  // 在tm记一遍：每个reducer都需要返回默认值，因为有内部初始化渲染dispatch的需要！
+  // counter: { number: 0 }, todo: { list: [] }
+  (state = {}, action) => { // 必须返回一个reducer(es6写法，买买，这种写法好语法糖啊！！！)
+    // if (!action) return state;
+    let newState = {};
+    for (var key in reducers) { // todo, counter 两个key
+      // newState[key] = reducers[key]; // 不能这样写，因为reducers[key]取来的是reducer，不是state啊。
+      // reducers[key](state[key], action); 我们不能传整个state进去，我们只能传key对应的老的state传进去
+      newState[key] = reducers[key](state[key], action);
+    }
+    return newState;
+  }
+
+export default combineReducers;
--- redux/CH06/src/store/store.js
+++ redux/CH06/src/store/store.js
@@ -1,5 +1,5 @@
 import { createStore } from '../redux';
-
+import combineReducers from './combineReducers';
 /**
  * 旧状态 {number:0}, {list:[]}
  * 新状态 { counter: {number:0}, todo: {list:[]} }
@@ -7,21 +7,6 @@ import { createStore } from '../redux';
 
 import counter from './reducers/counter';
 import todo from './reducers/todo';
-
-// 此时虽然我们有两个state的管理员reducer，但是createStore没法同时传入两个reducer，所以我们得给他们俩个找个“爹”。
-// 高阶函数
-let combineReducers = (reducers) =>
-  // 在tm记一遍：每个reducer都需要返回默认值，因为有内部初始化渲染dispatch的需要！
-  (state = { counter: { number: 0 }, todo: { list: [] } }, action) => { // 必须返回一个reducer(es6写法，买买，这种写法好语法糖啊！！！)
-    if(!action) return state;
-    let newState = {};
-    for (var key in reducers) { // todo, counter 两个key
-      // newState[key] = reducers[key]; // 不能这样写，因为reducers[key]取来的是reducer，不是state啊。
-      newState[key] = reducers[key](state[key], action);
-    }
-    return newState;
-  }
-
 
 // 这里用了es6写法
 let reducer = combineReducers({ counter, todo });
