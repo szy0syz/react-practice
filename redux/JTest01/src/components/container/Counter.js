48e0f36374f9aaeaa96a726e133ea668e69d7f9a
szy0syz <szy0syz@gmail.com>
2017-10-10 17:05:46 +0800

    updated JTest01 哈哈哈 终于搞清爽

--- redux/JTest01/src/components/container/Counter.js
+++ redux/JTest01/src/components/container/Counter.js
@@ -13,9 +13,10 @@ let mapStateToProps = (state) => ({
   displayNumber: state.number
 });
 
+// 注意：这里key的值必须是函数，丢事件嘛，不放函数放什么。
 let mapDispatchToProps = (dispatch) => ({
-  onADD: dispatch({ type: "ADD", amount: 5 }),
-  onSUB: dispatch({ type: "ADD", amount: 3 })
+  onADD: () => dispatch({ type: "ADD", amount: 5 }),
+  onSUB: () => dispatch({ type: "ADD", amount: 3 })
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Operator)
