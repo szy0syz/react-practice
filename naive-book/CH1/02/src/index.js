88bf44801276319d66e5d854f8ac609d29013d63
szy0syz <szy0syz@gmail.com>
2017-10-23 17:10:21 +0800

    update 升级demo1，具有组件化

--- naive-book/CH1/02/src/index.js
+++ naive-book/CH1/02/src/index.js
@@ -1,11 +1,48 @@
-const button = document.querySelector('.like-btn')
-const buttonText = button.querySelector('.like-text')
-let isLiked = false
-button.addEventListener('click', () => {
-  isLiked = !isLiked
-  if (isLiked) {
-    buttonText.innerHTML = '取消'
-  } else {
-    buttonText.innerHTML = '点赞'
+// const button = document.querySelector('.like-btn')
+// const buttonText = button.querySelector('.like-text')
+// let isLiked = false
+// button.addEventListener('click', () => {
+//   isLiked = !isLiked
+//   if (isLiked) {
+//     buttonText.innerHTML = '取消'
+//   } else {
+//     buttonText.innerHTML = '点赞'
+//   }
+// }, false)
+
+const createDOMFromString = (domString) => {
+  const div = document.createElement('div');
+  div.innerHTML = domString;
+  div.style.display = 'inline';
+  return div;
+}
+
+class LikeButton {
+  constructor() {
+    this.state = { isLiked: false }
   }
-}, false)
+
+  changeLikeText() {
+    const likeText = this.el.querySelector('.like-text')
+    this.state.isLiked = !this.state.isLiked
+    likeText.innerHTML = this.state.isLiked ? '取消' : '点赞'
+  }
+
+  render() {
+    this.el = createDOMFromString(`
+    <button class='like-button'>
+      <span class='like-text'>点赞</span>
+      <span>👍</span>
+    </button>
+  `)
+    this.el.addEventListener('click', this.changeLikeText.bind(this), false)
+    return this.el
+  }
+}
+
+const wraper = document.querySelector('.wrapper');
+const likeButton1 = new LikeButton();
+wraper.appendChild(likeButton1.render());
+
+const likeButton2 = new LikeButton();
+wraper.appendChild(likeButton2.render());
--- naive-book/README.md
+++ naive-book/README.md
@@ -1,3 +1,19 @@
 # React.js小书 练习
 
-## CH1 - 01前端组件化（一）：从一个简单的例子讲起
+## CH1 - 02前端组件化（一）：从一个简单的例子讲起
+
+### demo1
+
+初级原理实现：
+  1. 用dom获取button对象
+  2. 用dom获取button里文字对象
+  3. 在button对象上监听点击事件，toggle文字对象的值
+
+组件化升级实现：
+  1. 创建`LikeButton`的类，里面有个render方法，返回元素
+  2. 在类的`constructor`里初始化状态
+  3. 构建组件自身内部方法`changeLikeText`
+  4. 创建公用方法`createDOMFromString`：字符串转换成
+  5. render方法里将字符串传递给`createDOMFromString`方法，其返回一个被div包裹的dom元素
+  6. 将这个元素绑定响应事件
+  6. 用`render`方法返回这个元素
